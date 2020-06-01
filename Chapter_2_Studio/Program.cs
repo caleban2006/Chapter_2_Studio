55282705d4c1df63124d80e652605a6bbf70d52d
caleban2006 <logan.jen.2005@gmail.com>
2020-06-01 11:54:36 -0400

    added radius calculation

--- Chapter_2_Studio/Program.cs
+++ Chapter_2_Studio/Program.cs
@@ -6,7 +6,16 @@ namespace Chapter_2_Studio
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a radius of a circle:");
+
+            string userInput = Console.ReadLine();
+            double radius = double.Parse(userInput);
+            double area = Math.PI * Math.Pow(radius, 2);
+            double roundedArea = Math.Round(area, 3);
+
+            Console.WriteLine($"The area of a circle with a radius of {radius} is: {roundedArea}");
+
+
         }
     }
 }
