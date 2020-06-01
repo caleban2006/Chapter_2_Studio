07e85926bfb1cf8b8af4da0232121a3c6216d2d6
caleban2006 <logan.jen.2005@gmail.com>
2020-06-01 14:24:48 -0400

    still need bonus mission

---
+++ Chapter_2_Studio/Circle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter_2_Studio
+{
+    public class Circle
+    {
+        public static void GetCircle()
+        {
+
+                       
+        }
+    }
+}
--- Chapter_2_Studio/Program.cs
+++ Chapter_2_Studio/Program.cs
@@ -1,20 +1,32 @@
 ﻿using System;
+using System.Dynamic;
 
 namespace Chapter_2_Studio
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Enter a radius of a circle:");
+            
+            //Console.WriteLine("Enter a radius of a circle:");
 
-            string userInput = Console.ReadLine();
-            double radius = double.Parse(userInput);
-            double area = Math.PI * Math.Pow(radius, 2);
-            double roundedArea = Math.Round(area, 3);
+            //string userInput = Console.ReadLine();
+            //double radius = double.Parse(userInput);
+            //double area = Math.Round(Math.PI * Math.Pow(radius, 2), 3);
+            //double circumference = Math.Round((2 * Math.PI * radius), 3);
+            //double diameter = Math.Round((2 * radius), 3);
 
-            Console.WriteLine($"The area of a circle with a radius of {radius} is: {roundedArea}");
+            //Console.WriteLine($"The area of this circle is: {area}");
+            //Console.WriteLine($"The circumference this circle is: {circumference}");
+            //Console.WriteLine($"The diameter this circle is: {diameter}");
 
+            //Console.WriteLine("Please enter the miles per gallon that your vehicle uses:");
+
+            //string mpgUserInput = Console.ReadLine();
+            //double mpg = double.Parse(mpgUserInput);
+            //double gasUsed = Math.Round(circumference / mpg, 3);
+
+            //Console.WriteLine($"You would use {gasUsed} gallons of gas!");
 
         }
     }
